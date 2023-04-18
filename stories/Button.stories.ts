13f93b2ecd41698e7f490031bf51bb653a80f8bb
jackblatch <jack@26385.xyz>
2023-04-18 22:44:37 +0100

    add heading and button stories

--- stories/Button.stories.ts
+++ stories/Button.stories.ts
@@ -1,12 +1,12 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from "@storybook/react";
 
-import { Button } from '../components/ui/button';
+import { Button } from "../components/ui/button";
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 const meta: Meta<typeof Button> = {
-  title: 'Example/Button',
+  title: "Example/Button",
   component: Button,
-  tags: ['autodocs'],
+  tags: ["autodocs"],
   argTypes: {},
 };
 
@@ -14,29 +14,10 @@ export default meta;
 type Story = StoryObj<typeof Button>;
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
-export const Primary: Story = {
+export const Default: Story = {
   args: {
-    primary: true,
-    label: 'Button',
-  },
-};
-
-export const Secondary: Story = {
-  args: {
-    label: 'Button',
-  },
-};
-
-export const Large: Story = {
-  args: {
-    size: 'large',
-    label: 'Button',
-  },
-};
-
-export const Small: Story = {
-  args: {
-    size: 'small',
-    label: 'Button',
+    variant: "default",
+    size: "default",
+    children: "Button",
   },
 };
---
+++ stories/Heading.stories.ts
@@ -0,0 +1,41 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Heading } from "../components/ui/heading";
+
+const meta: Meta<typeof Heading> = {
+  title: "Example/Heading",
+  component: Heading,
+  tags: ["autodocs"],
+  argTypes: {},
+};
+
+export default meta;
+type Story = StoryObj<typeof Heading>;
+
+export const H1: Story = {
+  args: {
+    children: "Heading 1",
+    size: "h1",
+  },
+};
+
+export const H2: Story = {
+  args: {
+    children: "Heading 2",
+    size: "h2",
+  },
+};
+
+export const H3: Story = {
+  args: {
+    children: "Heading 3",
+    size: "h3",
+  },
+};
+
+export const H4: Story = {
+  args: {
+    children: "Heading 4",
+    size: "h4",
+  },
+};
