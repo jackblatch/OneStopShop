117696133eacf75dc5d28514d653c38b3c6164d2
jackblatch <me@jackblatch.com>
2023-05-26 09:21:04 +0100

    fix: stripe connectedAccount creation

--- server-actions/stripe.ts
+++ server-actions/stripe.ts
@@ -1,22 +1,30 @@
 "use server";
+import stripeDetails from "stripe";
 
 export async function createConnectedAccount() {
-  const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+  // @ts-ignore
+  const stripe = stripeDetails(process.env.STRIPE_SECRET_KEY);
+  try {
+    const account = await stripe.accounts.create({
+      type: "standard",
+    });
 
-  const account = await stripe.accounts.create({
-    type: "standard",
-  });
+    console.log(account);
 
-  console.log(account);
+    const accountLink = await stripe.accountLinks.create({
+      account: account.id,
+      refresh_url: "https://example.com/reauth",
+      return_url: "https://example.com/return",
+      type: "account_onboarding",
+    });
 
-  //   const accountLink = await stripe.accountLinks.create({
-  //     account: "{{CONNECTED_ACCOUNT_ID}}",
-  //     refresh_url: "https://example.com/reauth",
-  //     return_url: "https://example.com/return",
-  //     type: "account_onboarding",
-  //   });
+    console.log({ accountLink });
+  } catch (err) {
+    console.log("Error", err);
+  }
 
+  //   console.log({ accountLink });
   // save account id in database
 
-  return account;
+  //   return account;
 }
