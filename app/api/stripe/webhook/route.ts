0d3a27df851b9afe0473f0e8bd2d138030a696a8
jackblatch <me@jackblatch.com>
2023-06-02 11:38:19 +0100

    empty cart in webhook

--- app/api/stripe/webhook/route.ts
+++ app/api/stripe/webhook/route.ts
@@ -63,6 +63,7 @@ export async function POST(request: Request) {
           .update(carts)
           .set({
             isClosed: true,
+            items: JSON.stringify([]),
           })
           .where(eq(carts.paymentIntentId, paymentIntentId));
       } catch (err) {
