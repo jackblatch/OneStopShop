480d22ace0469b14603a4df6edb7d61d2eee6a00
jackblatch <me@jackblatch.com>
2023-06-11 19:55:45 +0100

    add further styling to homepage and global elements

--- app/(storefront)/(main)/page.tsx
+++ app/(storefront)/(main)/page.tsx
@@ -11,6 +11,18 @@ import { ProductCard } from "@/components/storefront/product-card";
 import { Button } from "@/components/ui/button";
 import { routes } from "@/lib/routes";
 import Link from "next/link";
+import { FeatureBanner } from "../components/feature-banner";
+import {
+  AlarmClock,
+  DollarSign,
+  FastForward,
+  Phone,
+  Truck,
+  User,
+  Wind,
+} from "lucide-react";
+import { Input } from "@/components/ui/input";
+import { TextInputWithLabel } from "@/components/text-input-with-label";
 
 export default async function Home() {
   const storeAndProduct = (await db
@@ -24,7 +36,7 @@ export default async function Home() {
     })
     .from(products)
     .leftJoin(stores, eq(products.storeId, stores.id))
-    .limit(4)) as ProductAndStore[];
+    .limit(8)) as ProductAndStore[];
 
   return (
     <div>
@@ -46,7 +58,30 @@ export default async function Home() {
                   over 1 million visitors.
                 </Heading>
               }
-            ></HomePageLayout>
+            >
+              <div className="md:grid md:grid-cols-3 gap-4 flex flex-col mt-12">
+                <FeatureBanner
+                  heading="No monthly fees"
+                  subheading="Fugit voluptates nihil ex et voluptas dignissimos blanditiis. Consectetur velit pariatur nihil quis nihil similique voluptatum in. Et nostrum ipsam quo magni. Velit et odit dolores."
+                  icon={<DollarSign size={32} />}
+                />
+                <FeatureBanner
+                  heading="Access to millions of buyers"
+                  subheading="Fugit voluptates nihil ex et voluptas dignissimos blanditiis. Consectetur velit pariatur nihil quis nihil similique voluptatum in. Et nostrum ipsam quo magni. Velit et odit dolores."
+                  icon={<User size={32} />}
+                />
+                <FeatureBanner
+                  heading="Quick and easy setup"
+                  subheading="Fugit voluptates nihil ex et voluptas dignissimos blanditiis. Consectetur velit pariatur nihil quis nihil similique voluptatum in. Et nostrum ipsam quo magni. Velit et odit dolores."
+                  icon={<AlarmClock size={32} />}
+                />
+              </div>
+              <div className="flex items-center justify-center mt-12">
+                <Link href={routes.signUp}>
+                  <Button size="lg">Create account</Button>
+                </Link>
+              </div>
+            </HomePageLayout>
           </TabsContent>
           <TabsContent value="for-buyers">
             <HomePageLayout
@@ -58,7 +93,7 @@ export default async function Home() {
               }
             >
               <Heading size="h3">Top Picks</Heading>
-              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 overflow-auto mt-2">
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 overflow-auto mt-4">
                 {storeAndProduct.map((item) => (
                   <ProductCard
                     key={item.product.id}
@@ -71,6 +106,39 @@ export default async function Home() {
                 <Link href={routes.products}>
                   <Button variant="default">View All Products</Button>
                 </Link>
+              </div>
+              <div className="bg-blue-900 text-white w-full p-12 rounded-md mt-12 flex items-center flex-col gap-2 justify-center text-center">
+                <p className="uppercase tracking-wide text-sm font-medium">
+                  Featured seller
+                </p>
+                <p className="text-3xl font-bold">Tim's Terrific Toys</p>
+                <p>
+                  Top seller of the month! Tim's Toys has been selling toys for
+                  10 years and is a top rated seller on the platform.
+                </p>
+                <Link
+                  href={routes.products + "?seller=tims-toys"}
+                  className="mt-6"
+                >
+                  <Button variant="secondary">Explore seller</Button>
+                </Link>
+              </div>
+              <div className="md:grid md:grid-cols-3 gap-4 flex flex-col mt-12">
+                <FeatureBanner
+                  heading="Free Shipping"
+                  subheading="Free shipping on all orders over $50"
+                  icon={<Truck size={32} />}
+                />
+                <FeatureBanner
+                  heading="24/7 Customer Support"
+                  subheading="Have a question? Get in touch."
+                  icon={<Phone size={32} />}
+                />
+                <FeatureBanner
+                  heading="Best prices"
+                  subheading="We offer the best prices on the market."
+                  icon={<DollarSign size={32} />}
+                />
               </div>
             </HomePageLayout>
           </TabsContent>
