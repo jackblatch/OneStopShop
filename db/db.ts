def952ac9811a2d60cb320439d1227ac88078719
Kamaliki Clive <47914903+kamaliki@users.noreply.github.com>
2023-11-01 21:01:34 +0300

    drizzle things

--- db/db.ts
+++ db/db.ts
@@ -1,19 +1,40 @@
 // db.ts
-import { drizzle } from "drizzle-orm/planetscale-serverless";
-import { connect } from "@planetscale/database";
+import { drizzle } from "drizzle-orm/mysql2";
+//import { connect } from "@planetscale/database";
+import { createPool, PoolConnection } from "mysql2/promise";
 import { migrate } from "drizzle-orm/mysql2/migrator";
 
-// create the connection
-const connection = connect({
-  host: process.env["DATABASE_HOST"],
-  username: process.env["DATABASE_USERNAME"],
-  password: process.env["DATABASE_PASSWORD"],
-});
+let db: any;
 
-export const db = drizzle(connection);
+// create the connection
+async function connectToMySQL() {
+  const connection = await createPool({
+    host: process.env["DATABASE_HOST"],
+    user: process.env["DATABASE_USERNAME"],
+    password: process.env["DATABASE_PASSWORD"],
+    database: process.env["DATABASE_NAME"],
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+  });
+  return connection;
+}
+
+export async function initializeDB(){
+ const connection = await connectToMySQL();
+ const db = drizzle(connection);
 
 // syncs the migrations folder to PlanetScale
 process.env.NODE_ENV === "development" &&
   migrate(db as any, { migrationsFolder: "./migrations-folder" })
     .then((res) => res)
     .catch((err) => console.log("Migration error in db.ts:", err));
+
+}
+
+initializeDB();
+
+export function getDB() {
+  return db;
+}
+
