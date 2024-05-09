7e6404062314c5fbab6479fd21cc02680658b82d
jackblatch <me@jackblatch.com>
2024-05-09 20:45:16 +0100

    use pool

--- db/db.ts
+++ db/db.ts
@@ -3,7 +3,7 @@ import { drizzle } from "drizzle-orm/mysql2";
 import mysql from "mysql2/promise";
 
 // create the connection
-const connection = await mysql.createConnection({
+const connection = mysql.createPool({
   uri: process.env.DATABASE_URL,
 });
 
